1116838de7e3a9391ee158d1fc3ea0a7bb91521d
Saad Umar <112772287+SAADUMAR121@users.noreply.github.com>
2024-01-09 03:16:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['923321709439','923321709439'] 
+global.prems = ['923321709439', '923321709439', '923321709439']
+global.allowed = ['923321709439', '923321709439', '923321709439']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,17 +50,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ Hunter Bot'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.packname = 'HUNTER┃ᴮᴼᵀ' 
+global.author = '@Baloch' 
+global.menuvid = 'https://i.imgur.com/Fpt8sgB.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/saadumar.650\n' 
+global.dygp = 'https://chat.whatsapp.com/Fc97ajkg5DvBbSGYfmRU8M'
+global.fgsc = 'https://github.com/SAADUMAR121/GURU-BOT' 
+global.fglog = 'https://graph.org/file/56c0fb63b1eb1b2ca789c.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
