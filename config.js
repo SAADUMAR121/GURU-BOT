9df05003dc4bf139d698afdf2110099b25138da1
Saad Umar <112772287+SAADUMAR121@users.noreply.github.com>
2024-01-20 00:50:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923321709439', 'SAAD UMAR', true],
   [''] 
 ] //Number of owners
 
